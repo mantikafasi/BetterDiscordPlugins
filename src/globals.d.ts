7eef79655294302d344495f06051160a1eb28814
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-05-14 22:40:01 +0300

    Initial commit

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+declare global {
+    export const BdApi: any;
+    export const DiscordNative: any;
+}
+
+export {};
---
+++ src/plugins/SendTimestamps/index.jsx
@@ -0,0 +1,31 @@
+import { ChatBarComponent } from "./modal";
+import { findInReactTree } from "../../shared/findInReactTree";
+
+import styles from "~fileContent/styles.css";
+
+const Chat = BdApi.Webpack.getModule(m => m.Z?.type?.render?.toString().includes("chat input type must be set"));
+
+function start() {
+    BdApi.DOM.addStyle("send-timestamps", styles);
+
+    const unpatchOuter = BdApi.Patcher.after("send-timestamps", Chat.Z.type, "render", (_this, _args, res) => {
+        unpatchOuter();
+
+        const inner = findInReactTree(res, n => n?.props?.className?.includes("sansAttachButton-"));
+
+        BdApi.Patcher.after("send-timestamps", inner.props.children[2].type, "type", (_this2, [props], buttonsRes) => {
+            if (props.disabled) return;
+            buttonsRes.props.children.unshift(<ChatBarComponent />);
+        });
+    });
+}
+
+function stop() {
+    BdApi.DOM.removeStyle("send-timestamps");
+    BdApi.Patcher.unpatchAll("send-timestamps");
+}
+
+module.exports = () => ({
+    start,
+    stop
+});
